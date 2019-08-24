6c6855d654fdc7de53925d886a41c5d7e107b30d
Rian Fadli <rianfadli11@gmail.com>
2019-08-24 13:15:59 +0700

    oke

--- simpan_buku.php
+++ simpan_buku.php
@@ -7,9 +7,12 @@ $kategori 		= $_POST['kategori'];
 $deskripsi 		= $_POST['deskripsi'];
 
 include 'koneksi.php';
-$sql = "INSERT into buku SET isbn='$isbn',judul_buku='judul_buku',
-		id_kategori='$kategori',id_penulis='p$penulis',deskripsi='$deskripsi'";
+$sql = "INSERT into buku SET isbn='$isbn',judul_buku='$judul_buku',
+		id_kategori='$kategori',id_penulis='$penulis',deskripsi='$deskripsi'";
 $insert = mysqli_query($konek, $sql);
+
+//print_r($_POST);
+//die;
 if($insert){
 	echo "berhasil";
 }else{
