279da65538fab9bac1102934b1f12762e314b714
Rian Fadli <rianfadli11@gmail.com>
2019-08-27 11:28:39 +0700

    ok

--- delete_buku.php
+++ delete_buku.php
@@ -1,9 +1,13 @@
 <?php
 $isbn = isset($_GET['isbn']);
+
 if($isbn){
+
 	include 'koneksi.php';
-	$delete = mysqli_query($konek, "DELETE FROM buku WHERE isbn='$isbn' ");
+	$delete = mysqli_query($konek, "DELETE FROM buku WHERE isbn='$isbn'");
+
 }else{
+
 	echo "Data Gagal Di Simpan";
 }
 
